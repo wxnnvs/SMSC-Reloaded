92ed698ac0ee598a9a0364a14392d5be202d0e2f
wxnnvs <215158@proton.me>
2023-10-08 23:39:50 +0200

    add toggling
    made css load from extension url
    you can use ctrl+K to toggle darkmode

--- extension/inject.js
+++ extension/inject.js
@@ -1,3 +1,25 @@
-fetch('https://raw.githubusercontent.com/wxnnvs/SMSC-Reloaded/main/extension/style.css')
-  .then(response => response.text())
-  .then(response => document.body.insertAdjacentHTML("beforeend", "<style>" + response + "</style>"));
+function toggleStylesheet( href, onoff ){
+  var existingNode=0 //get existing stylesheet node if it already exists:
+  for(var i = 0; i < document.styleSheets.length; i++){
+      if( document.styleSheets[i].href && document.styleSheets[i].href.indexOf(href)>-1 ) existingNode = document.styleSheets[i].ownerNode
+  }
+  if(onoff == undefined) onoff = !existingNode //toggle on or off if undefined
+  if(onoff){ //TURN ON:
+      if(existingNode) return onoff //already exists so cancel now
+      var link  = document.createElement('link');
+      link.rel  = 'stylesheet';
+      link.type = 'text/css';
+      link.href = href;
+      document.getElementsByTagName('head')[0].appendChild(link);
+  }else{ //TURN OFF:
+      if(existingNode) existingNode.parentNode.removeChild(existingNode)
+  }
+  return onoff
+}
+
+toggleStylesheet(chrome.runtime.getURL('style.css'), 1);
+document.addEventListener('keydown', (event) => {
+  if (event.ctrlKey && event.key === 'k'){
+    toggleStylesheet(chrome.runtime.getURL('style.css'));
+  }
+})
